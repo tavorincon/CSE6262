2ae2f39ac50a0c57471b15be2908c030ba1b6561
Martín R. Niño <martin.r.nino@gmail.com>
2021-12-02 09:13:23 -0600

    Add police allocation and historical analysis screens

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ The project uses MapBox for obtaining the map tiles. You need to generate an [Ac
 
 ### Obtaining geoJSON boundaries
 For displaying the ZIP Codes polygons we obtained the boundaries from a freemium API in RapidAPI. If you want to refresh the data you need to create an API key [here](https://rapidapi.com/VanitySoft/api/boundaries-io-1/), then copy and paste it in the `.env` file.
+
+## Credits
+- Ismael Montes
+- Gustavo Rincon
+- Mauricio J Diaz
+- Juan C Pineda
+- Martin Rodriguez
--- main.py
+++ main.py
@@ -19,6 +19,24 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/historical-crime")
+# Agregar filtro por fecha y el default seria un año hacia atras (para el mapa 3D por hexagono)
+async def historical_data(days: Optional[int] = 365):
+    # Cambiar el CSV por el query
+    with open("data/crime/test_latlon.csv", 'r') as f:
+        return f.read()
+
+@app.get("/officer-allocation")
+# Regresa la predicción del numero de crimenes y la asignación propuesta de oficiales (para pintar el mapa por zipcode)
+async def historical_data(days: Optional[int] = 365):
+    # Cambiar el CSV por el query
+    return None
+
+@app.get("/crime-prediction")
+# Regresa la predicción del numero de crimenes y la asignación propuesta de oficiales (para pintar el timeseries por zipcode en el hover)
+async def historical_data(zip_code: str):
+    # Cambiar el CSV por el query
+    return None
 
 ################################################################################
 # Front End
@@ -26,11 +44,20 @@ def read_root():
 templates = Jinja2Templates(directory="frontend/templates")
 
 
-@app.get("/map", response_class=HTMLResponse)
+@app.get("/allocation", response_class=HTMLResponse)
 async def show_map(request: Request):
     return templates.TemplateResponse(
-        "sample-choroplet.html",
-        {"request": request, "mapbox_key": os.getenv("MAPBOX_KEY")},
+        "allocation.html",
+        {"request": request, "segment": "allocation", "mapbox_key": os.getenv("MAPBOX_KEY")},
+    )
+
+
+@app.get("/historical", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "historical.html",
+        {"request": request,
+        "segment": "historical"},
     )
 
 @app.get("/dash", response_class=HTMLResponse)
